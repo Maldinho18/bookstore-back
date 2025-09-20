cb2566625b7f1df4bf7d620132e05ef59bb7c176
Maldo <js.maldonado2@uniandes.edu.co>
2025-09-20 00:05:27 -0500

    amen

--- lib/zodSchemas.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const authorSchema = z.object({
-    name: z.string().min(2),
-    birthDate: z.string(),
-    description: z.string().min(10),
-    image: z.string().url("URL de imagen no válida"),
-});
-export type AuthorFormValues = z.infer<typeof authorSchema>;
