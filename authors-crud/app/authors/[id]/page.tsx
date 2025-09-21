157fa3ac01bec9d79f645c5aaa7494f4c090bb0a
Maldo <js.maldonado2@uniandes.edu.co>
2025-09-21 12:39:22 -0500

    error minimo

--- authors-crud/app/authors/[id]/page.tsx
+++ authors-crud/app/authors/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { getAuthor } from "@/lib/api";
 import EditClient from "./_EditClient";
 
-export default async function EditAuthorPage({ params }: { params: { id: string } }) {
-    const author = await getAuthor(Number(params.id));
+export default async function EditAuthorPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const author = await getAuthor(Number(id));
     return <EditClient initial={author} />;
 }
